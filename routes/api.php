1fabba0dd3abb33e2b1df1b84f302849dcdd9b94
Charmy Patel <mi.charmypatel@gmail.com>
2025-09-28 15:23:08 +0530

    removed unneccesary route

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UploadController;
-
-
-Route::post('/uploads/chunk', [UploadController::class, 'uploadChunk']);
-Route::post('/uploads/complete', [UploadController::class, 'completeUpload']);
